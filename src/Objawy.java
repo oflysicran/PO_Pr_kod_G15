35428af4122f213d97f8c9968d6b0608cc9fb480
areklt <areklt@proton.me>
2025-05-25 01:45:04 +0200

    Update Objawy.java

--- src/Objawy.java
+++ src/Objawy.java
@@ -35,13 +35,41 @@ public class Objawy{
         lista.add(new Objawy(kat, 3, "Otwiera na polecenie"));
         lista.add(new Objawy(kat, 2, "Otwiera na ból"));
         lista.add(new Objawy(kat, 1, "Oczy zamknięte"));
+        break;
+      }
+      case werbalna:{
+        lista.add(new Objawy(kat, 5, "Pełna orientacja"));
+        lista.add(new Objawy(kat, 4, "Zdezorientowany"));
+        lista.add(new Objawy(kat, 3, "Mowa niezrozumiała"));
+        lista.add(new Objawy(kat, 2, "Dźwięki"));
+        lista.add(new Objawy(kat, 1, "Brak dźwięków"));
+        break;
+      }
+      case ruchowa:{
+        lista.add(new Objawy(kat, 6, "Zgodne z poleceniem"));        
+        lista.add(new Objawy(kat, 5, "Celowe cofnięcie"));
+        lista.add(new Objawy(kat, 4, "Odruch cofnięcia"));
+        lista.add(new Objawy(kat, 3, "Sztywność zgięciowa"));
+        lista.add(new Objawy(kat, 2, "Sztywność wyprostna"));
+        lista.add(new Objawy(kat, 1, "Brak ruchu"));
+        break;
+      }
+    }
+    return lista.get(new Random().nextInt(lista.size()));
+  }
 
+  public static List<Objawy> generujLosowe(){
+    List<Objawy> wynik = new ArrayList<>();
+    wynik.add(losowy(Kategoria.oczy));
+    wynik.add(losowy(Kategoria.werbalna));
+    wynik.add(losowy(Kategoria.ruchowa));
+    return wynik;
+  }
 
-
-
-
-
-
-
-
-  
+  public static StanPacjenta przypiszStan(List<Objawy> objawy){
+    int suma = objawy.stream().mapToInt(Objawy::wartoscPunktowa).sum();
+    if (suma >= 13) return StanPacjenta.Niebieski;
+    if (suma >= 9) return StanPacjenta.Zolty;
+    return StanPacjenta.czerwony;
+  }
+}
