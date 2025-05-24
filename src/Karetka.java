4f3387e0a6a30670e384c1e28999eec71fe4dd76
areklt <areklt@proton.me>
2025-05-25 00:54:57 +0200

    Update Karetka.java

--- src/Karetka.java
+++ src/Karetka.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+public class Karetka{
+  private final List<Pacjent> pacjenci = new ArrayList<>();
+  private final Random los = new Random();
+
+  public void nowaTura(int turaId){
+    int szansa = los.nextInt(100);
+    if (szansa < 30) {
+      przyjazd(turaId);
+    }
+  }
+
+private void przyjazd(int turaId){
+  int liczbaPacjentow = 1 + los.nextInt(xxx);
+  for (int i = 1; i < liczbaPacjentow; i++){
+    int id = ;// nie znam metody
