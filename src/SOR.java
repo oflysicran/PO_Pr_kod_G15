55038304db82a8759bd7c3062b843ead5d889918
oflysicran <nikola.wieczorkiewicz05@gmail.com>
2025-05-25 01:00:25 +0200

    Update SOR.java

--- src/SOR.java
+++ src/SOR.java
@@ -1,12 +1,9 @@
 package po_pr_kod_g15;
 
 public class SOR {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
