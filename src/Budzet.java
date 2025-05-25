d662eda0f87af43078b382f086b0997c3074bb4b
oflysicran <nikola.wieczorkiewicz05@gmail.com>
2025-05-25 21:51:43 +0200

    Update and rename Budżet.java to Budzet.java

---
+++ src/Budzet.java
@@ -0,0 +1,11 @@
+package po_pr_kod_g15;
+
+public class Budżet {
+  private double sumaStartowa;
+  private double aktualnyBudzet;
+
+  public Budzet(sumaStartowa) {
+    this.sumaStartowa = sumaStartowa;
+    this.aktualnyBudzet = sumaStartowa
+  }
+}
