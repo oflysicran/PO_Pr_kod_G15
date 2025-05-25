e1ef04e7ee06aa4231de4f651317860c6c44397a
oflysicran <nikola.wieczorkiewicz05@gmail.com>
2025-05-25 23:16:28 +0200

    Update Pacjent.java

--- src/Pacjent.java
+++ src/Pacjent.java
@@ -1,12 +1,14 @@
 package po_pr_kod_g15;
 
 public class Pacjent extends Osoba {
-    protected enum stan {
-        Niebieski, Zolty, Czerwony;
+    public enum StanPacjenta {
+        NIEBIESKI, ŻÓŁTY, CZERWONY
     }
+    protected StanPacjenta stan;
     
-    public Pacjent() {
-        super();
+    public Pacjent(int id, String imie, String nazwisko, StanPacjenta stan) {
+        super(id, imie, nazwisko, TypOsoby.PACJENT);
+        this.stan = stan;
     }
 
     @Override
