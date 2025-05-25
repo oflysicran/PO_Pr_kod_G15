6b70a8a666f4d4fa370292fda628a8c9256d8415
oflysicran <nikola.wieczorkiewicz05@gmail.com>
2025-05-25 21:41:51 +0200

    Update Pacjent.java

--- src/Pacjent.java
+++ src/Pacjent.java
@@ -4,4 +4,13 @@ public class Pacjent extends Osoba {
     protected enum stan {
         Niebieski, Zolty, Czerwony;
     }
+    
+    public Pacjent() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Pacjent - ID: " + sformatowaneId() + " - " + imie + " " + nazwisko;
+    }
 }
