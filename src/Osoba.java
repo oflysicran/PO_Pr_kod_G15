904973285b8609a811a3fea44b3506057221511b
oflysicran <nikola.wieczorkiewicz05@gmail.com>
2025-05-25 19:14:19 +0200

    Update Osoba.java
    Tworzenie id (0000-9999)
    Klasa osoba jest klasą abstrakcyjną

--- src/Osoba.java
+++ src/Osoba.java
@@ -1,10 +1,34 @@
 package po_pr_kod_g15;
+import java.util.HashSet; // do przechowywania już użytych ID, 
+import java.util.Set;     // żeby każde nowe ID było unikalne.
+import java.util.Random;
 
 public abstract class Osoba {
-    protected int id;
-    
-    protected enum typ {
-        Personel, Pacjent;
+    static final Set<Integer> usedIds = new HashSet<>();
+    static final Random random = new Random();
+
+    public final int id;
+
+    public Osoba() {
+        this.id = generateUniqueId();
     }
-    
+
+    private int generateUniqueId() {
+        if (usedIds.size() >= 9999) {
+            throw new RuntimeException("Wszystkie możliwe ID zostały wykorzystane.");
+        }
+
+        int newId;
+        do {
+            newId = 1 + random.nextInt(9999); // liczba z zakresu 1–9999
+        } while (usedIds.contains(newId));
+
+        usedIds.add(newId);
+        return newId;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + String.format("%04d", id);
+    }
 }
