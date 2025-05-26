687ea4727dd71585f1dc8c357e7ace357d6be446
maciekszotekidol <w_szymanskaa@proton.me>
2025-05-26 22:39:58 +0200

    Update StanPacjenta.java

--- po_pr_kod_g15/PO25_Projekt_Mini_WSzymanka_NWieczorkiewicz/src/po_pr_kod_g15/StanPacjenta.java
+++ po_pr_kod_g15/PO25_Projekt_Mini_WSzymanka_NWieczorkiewicz/src/po_pr_kod_g15/StanPacjenta.java
@@ -3,23 +3,28 @@ package po_pr_kod_g15;
 import java.util.ArrayList;
 import java.util.List;
 
-public enum StanPacjenta {
-    Niebieski,
-    Zolty,
-    Czerwony;
+public class StanPacjenta {
 
-    public static List<Objawy> generujLosowe() {
+    public enum Stan {
+        Niebieski,
+        Zolty,
+        Czerwony;
+
+        
+    }
+    
+    public static Stan przypiszStan(List<Objawy> objawy) {
+        int suma = objawy.stream().mapToInt(Objawy::ciezkoscWartosc).sum();
+        if (suma >= 13) return Stan.Niebieski;
+        if (suma >= 9) return Stan.Zolty;
+        return Stan.Czerwony;
+    }
+    
+    public static List<Objawy> generujLosoweObjawy() {
         List<Objawy> wynik = new ArrayList<>();
         wynik.add(Objawy.losowy(Objawy.Kategoria.oczy));
         wynik.add(Objawy.losowy(Objawy.Kategoria.werbalna));
         wynik.add(Objawy.losowy(Objawy.Kategoria.ruchowa));
         return wynik;
-    }
-
-    public static StanPacjenta przypiszStan(List<Objawy> objawy) {
-        int suma = objawy.stream().mapToInt(Objawy::ciezkoscWartosc).sum();
-        if (suma >= 13) return StanPacjenta.Niebieski;
-        if (suma >= 9) return StanPacjenta.Zolty;
-        return StanPacjenta.Czerwony;
     }
 }
