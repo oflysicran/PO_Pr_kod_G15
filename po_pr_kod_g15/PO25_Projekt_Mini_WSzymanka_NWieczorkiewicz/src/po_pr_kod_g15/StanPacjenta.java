b07deb44113d0260425ce429136e3702a7e39da4
maciekszotekidol <w_szymanskaa@proton.me>
2025-05-26 16:33:07 +0200

    Update StanPacjenta.java

--- po_pr_kod_g15/PO25_Projekt_Mini_WSzymanka_NWieczorkiewicz/src/po_pr_kod_g15/StanPacjenta.java
+++ po_pr_kod_g15/PO25_Projekt_Mini_WSzymanka_NWieczorkiewicz/src/po_pr_kod_g15/StanPacjenta.java
@@ -1,7 +1,26 @@
+// StanPacjenta.java
 package po_pr_kod_g15;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public enum StanPacjenta {
     Niebieski,
     Zolty,
-    Czerwony
+    Czerwony;
+
+    public static List<Objawy> generujLosowe() {
+        List<Objawy> wynik = new ArrayList<>();
+        wynik.add(Objawy.losowy(Objawy.Kategoria.oczy));
+        wynik.add(Objawy.losowy(Objawy.Kategoria.werbalna));
+        wynik.add(Objawy.losowy(Objawy.Kategoria.ruchowa));
+        return wynik;
+    }
+
+    public static StanPacjenta przypiszStan(List<Objawy> objawy) {
+        int suma = objawy.stream().mapToInt(Objawy::ciezkoscWartosc).sum();
+        if (suma >= 13) return StanPacjenta.Niebieski;
+        if (suma >= 9) return StanPacjenta.Zolty;
+        return StanPacjenta.Czerwony;
+    }
 }
