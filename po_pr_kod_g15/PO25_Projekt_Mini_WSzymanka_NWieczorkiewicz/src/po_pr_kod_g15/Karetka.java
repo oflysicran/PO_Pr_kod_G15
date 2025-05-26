bd16b7b73b57b7569bbc9b0e78c57170a3d292bd
maciekszotekidol <w_szymanskaa@proton.me>
2025-05-26 16:35:27 +0200

    Update Karetka.java

--- po_pr_kod_g15/PO25_Projekt_Mini_WSzymanka_NWieczorkiewicz/src/po_pr_kod_g15/Karetka.java
+++ po_pr_kod_g15/PO25_Projekt_Mini_WSzymanka_NWieczorkiewicz/src/po_pr_kod_g15/Karetka.java
@@ -23,8 +23,8 @@ public class Karetka {
         int liczbaPacjentow = 2 + los.nextInt(2); // 2–3 pacjentów
 
         for (int i = 0; i < liczbaPacjentow; i++) {
-            List<Objawy> objawy = Objawy.generujLosowe();
-            StanPacjenta stan = Objawy.przypiszStan(objawy);
+            List<Objawy> objawy = StanPacjenta.generujLosowe();
+            StanPacjenta stan = StanPacjenta.przypiszStan(objawy);
 
             // losowe imię i nazwisko pacjenta
             String imie = Osoba.losujImie();
